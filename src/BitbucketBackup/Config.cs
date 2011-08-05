1eedf6b5071671d4b98cf8bd164272f791ac2c7d
Christian Specht <christi@nspecht.de>
2011-08-06 00:43:35 +0200

    add comments

--- src/BitbucketBackup/Config.cs
+++ src/BitbucketBackup/Config.cs
@@ -2,10 +2,24 @@
 
 namespace BitbucketBackup
 {
+    /// <summary>
+    /// helper class for getting values out of the config file
+    /// </summary>
     internal class Config
     {
+        /// <summary>
+        /// Bitbucket username
+        /// </summary>
         public string UserName { get; private set; }
+
+        /// <summary>
+        /// Bitbucket password
+        /// </summary>
         public string PassWord { get; private set; }
+
+        /// <summary>
+        /// Folder on local machine where backups are saved
+        /// </summary>
         public string BackupFolder { get; private set; }
 
         public Config()
