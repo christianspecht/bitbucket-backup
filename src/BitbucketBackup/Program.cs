262f5983762fac9d54248cae4817b71c7d482048
Christian Specht <christi@nspecht.de>
2011-08-09 21:23:29 +0200

    parse the response with Json.NET and output the repository names to console (until we actually can do something real with them)

--- src/BitbucketBackup/Program.cs
+++ src/BitbucketBackup/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
 namespace BitbucketBackup
 {
     class Program
@@ -10,6 +13,22 @@ namespace BitbucketBackup
 
             string resource = "users/" + config.UserName;
             string response = request.Execute(resource);
+
+            if (response == string.Empty)
+            {
+                Console.WriteLine("Bitbucket API didn't return a response: " + resource);
+                Console.ReadLine();
+            }
+
+            var json = JObject.Parse(response);
+            var repos = json.SelectToken("repositories").Select(n => (string)n.SelectToken("slug")).ToList();
+
+            foreach (string repo in repos)
+            {
+                Console.WriteLine(repo);
+            }
+
+            Console.ReadLine();
         }
     }
 }
