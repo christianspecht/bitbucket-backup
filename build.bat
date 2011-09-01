c0d6d07572f287c901c9bf1235bc445f771c09fe
Christian Specht <christi@nspecht.de>
2011-09-02 00:22:16 +0200

    fix %PATH% handling in build script (append the MSBuild path to the %PATH% variable instead of overwriting it)

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem path to msbuild.exe
-path="%windir%\Microsoft.net\Framework\v4.0.30319"
+path=%path%;%windir%\Microsoft.net\Framework\v4.0.30319
 
 rem go to current folder
 cd %~dp0
