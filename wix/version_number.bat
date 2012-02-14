ddfad0ca477d436f15874293396d8a281650032b
Christian Specht <christi@nspecht.de>
2012-02-14 21:09:42 +0100

    set version number to 1.2.1

--- wix/version_number.bat
+++ wix/version_number.bat
@@ -4,8 +4,8 @@ rem Before releasing a new version, you must change BOTH values in this file as 
 
 rem Version Number:
 rem This number is used for both the setup AND the actual application. Just increase it.
-set VersionNumber=1.2
+set VersionNumber=1.2.1
 
 rem ProductId:
 rem Replace the GUID by a new one (important: with capital letters and curly braces!)
-set ProductId={2A579449-33D6-4962-B020-444684ECE30F}
+set ProductId={B3737757-1BB7-42FC-BD27-2DD11A472291}
