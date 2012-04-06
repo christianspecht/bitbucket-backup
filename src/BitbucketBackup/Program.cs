6d88954411cc00c2f6e28660d969e6fbab10c785
Christian Specht <christi@nspecht.de>
2012-04-06 17:24:55 +0200

    remove unnecessary usings

--- src/BitbucketBackup/Program.cs
+++ src/BitbucketBackup/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
-using Newtonsoft.Json.Linq;
-
+﻿
 namespace BitbucketBackup
 {
     class Program
