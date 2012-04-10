39d6d62fcab4adc71b7cac41898a82b65ae4a4fc
Christian Specht <christi@nspecht.de>
2012-04-10 18:14:27 +0200

    increase version number

--- wix/version_number.bat
+++ wix/version_number.bat
@@ -4,8 +4,8 @@ rem Before releasing a new version, you must change BOTH values in this file as 
 
 rem Version Number:
 rem This number is used for both the setup AND the actual application. Just increase it.
-set VersionNumber=1.2.1
+set VersionNumber=1.2.2
 
 rem ProductId:
 rem Replace the GUID by a new one (important: with capital letters and curly braces!)
-set ProductId={B3737757-1BB7-42FC-BD27-2DD11A472291}
+set ProductId={E3DE5A4B-C1D0-4DF5-9500-B954DFC27897}
