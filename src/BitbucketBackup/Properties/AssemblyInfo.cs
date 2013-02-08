6555b7618c7b3db74a3bc9fe65671037f14eed8f
Christian Specht <christi@nspecht.de>
2013-02-08 16:15:25 +0100

    update copyright to 2013

--- src/BitbucketBackup/Properties/AssemblyInfo.cs
+++ src/BitbucketBackup/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("christianspecht.de/bitbucket-backup")]
 [assembly: AssemblyProduct("Bitbucket Backup")]
-[assembly: AssemblyCopyright("Copyright © Christian Specht 2011-2012")]
+[assembly: AssemblyCopyright("Copyright © Christian Specht 2011-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
